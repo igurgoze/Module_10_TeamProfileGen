864f3be52afe2c80440ad56f3dcb25817f158995
Ian Gurgoze <kb8monkey@yahoo.com>
2023-01-26 00:38:13 -0700

    still need tests, setting up roles to create questions

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require ('inquirer');
+const fs = require('fs');
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+
+
+const managerQ = () => {
+    
+}
+const engineerQ = () => {
+    
+}
+const employeeQ = () => {
+    
+}
+const internQ = () => {
+    
+}
